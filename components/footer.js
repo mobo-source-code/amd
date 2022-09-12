1a1393d39cd99abdeccb397014fefeae4c6b916e
Mohammed Bouaziz <mobo.repos@gmail.com>
2022-09-12 17:20:24 +0100

    title

--- components/footer.js
+++ components/footer.js
@@ -3,15 +3,15 @@ import Image from "next/image"
 
 const Footer = () => {
     return (
-    <div className="w-full px-10 py-5 flex justify-between bg-primary h-24 items-center">
+    <div className="w-full px-10 py-0 md:py-5 flex flex-col md:flex-row justify-center md:justify-between bg-primary h-24 items-center">
             <p className="text-gray-700 font-head text-md">
             +212 6 97 91 50 51
         </p>
         
-        <p className="text-gray-700 font-head font-light text-md">
+        <p className="hidden md:block text-gray-700 font-head font-light text-md">
             L'Académie des métiers digitaux S.A.R.L
         </p>
-        <p className="text-gray-700 font-head text-md">
+        <p className="hidden md:block text-gray-700 font-head text-md">
             AMD
         </p>
     </div>
